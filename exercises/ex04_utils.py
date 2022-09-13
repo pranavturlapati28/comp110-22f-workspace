e166deb855c62bbaa8118e37432a18972a3ef8d0
Pranav Turlapati <pran@unc.edu>
2022-09-13 19:55:58 -0400

    Final Version

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,15 +1,15 @@
-"""In this prigram we will be understanding the fundamentals of list functions"""
+"""In this program we will be understanding the fundamentals of list functions."""
 
 __author__ = "730579095"
 
-from asyncore import loop
-
 
 def all(list_parameter: list[int], target_int: int) -> bool:
-    """This list returns true if all the values in a list are the same as the target integer"""
+    """Checks if all the values in a lost are equal to a target int."""
+    if len(list_parameter) == 0:
+        return False
     list_length: int = len(list_parameter)
     """The default condition for the function is True, 
-    if any on the indices do not equal the target int, it immediately returns False"""
+    if any on the indices do not equal the target int, it immediately returns False."""
     list_traverse_count: int = 0
     while list_traverse_count < list_length:
         if list_parameter[list_traverse_count] != target_int:
@@ -19,13 +19,13 @@ def all(list_parameter: list[int], target_int: int) -> bool:
 
 
 def max(list_parameter: list[int]) -> int:
-    """Returns the maximum of the given list"""
+    """Returns the maximum of the given list."""
     if len(list_parameter) == 0:
         raise ValueError("max() arg is an empty List")
     max_value = list_parameter[0]
     loop_traverse: int = 1
     """This function sets the first index to the max, and if any values are greater,
-    max is set to that value. The final max is returned"""
+    max is set to that value. The final max is returned."""
     while loop_traverse < len(list_parameter):
         if list_parameter[loop_traverse] > max_value:
             max_value = list_parameter[loop_traverse]
@@ -34,12 +34,15 @@ def max(list_parameter: list[int]) -> int:
 
 
 def is_equal(first_list: list[int], second_list: list[int]) -> bool:
-    "If the two lists are deeply equal, we only then return True"
+    """If the two lists are deeply equal, we only then return True."""
     loop_counter: int = 0
-    """The default condition is True, one loop counter compares the two list. If the same index value on both
-    lists dont match, it immediately returns false"""
-    while loop_counter < len(first_list):
-        if first_list[loop_counter] != second_list[loop_counter]:
-            return False
-        loop_counter += 1
+    """The default condition is True, one loop counter compares the two list. If the same index value on both,
+    lists dont match, it immediately returns false."""
+    if len(first_list) != len(second_list):
+        return False
+    else:
+        while loop_counter < len(first_list):
+            if first_list[loop_counter] != second_list[loop_counter]:
+                return False
+            loop_counter += 1
     return True
